8a737283e2207fc36c0b583cea3060454514715a
Ignacio Roncero <ironcero@cloudbees.com>
2026-04-17 11:27:10 +0200

    Changing javadocs

--- src/test/java/com/sonyericsson/hudson/plugins/gerrit/trigger/hudsontrigger/BuildCancellationIntegrationTest.java
+++ src/test/java/com/sonyericsson/hudson/plugins/gerrit/trigger/hudsontrigger/BuildCancellationIntegrationTest.java
@@ -117,7 +117,7 @@ public class BuildCancellationIntegrationTest {
      * Tests that a new patchset cancels a running build of an old patchset.
      * This is the most common cancellation scenario.
      *
-     * @throws Exception if the test faced issues running jobs or working with the System.
+     * @throws Exception if unexpected errors appear.
      */
     @Test
     @LocalData
@@ -180,7 +180,7 @@ public class BuildCancellationIntegrationTest {
     /**
      * Tests that abortNewPatchsets policy cancels even newer patchsets with older ones.
      *
-     * @throws Exception if the test faced issues running jobs or working with the System.
+     * @throws Exception if unexpected errors appear.
      */
     @Test
     @LocalData
@@ -230,7 +230,7 @@ public class BuildCancellationIntegrationTest {
     /**
      * Tests that builds in queue are canceled before they start.
      *
-     * @throws Exception if the test faced issues running jobs or working with the System.
+     * @throws Exception if unexpected errors appear.
      */
     @Test
     @LocalData
@@ -300,7 +300,7 @@ public class BuildCancellationIntegrationTest {
     /**
      * Tests that abandoned patchsets cancel running builds.
      *
-     * @throws Exception if the test faced issues running jobs or working with the System.
+     * @throws Exception if unexpected errors appear.
      */
     @Test
     @LocalData
@@ -350,7 +350,7 @@ public class BuildCancellationIntegrationTest {
     /**
      * Tests that different changes don't interfere with each other.
      *
-     * @throws Exception if the test faced issues running jobs or working with the System.
+     * @throws Exception if unexpected errors appear.
      */
     @Test
     @LocalData
@@ -403,7 +403,7 @@ public class BuildCancellationIntegrationTest {
     /**
      * Tests that policy disabled does not cancel builds.
      *
-     * @throws Exception if the test faced issues running jobs or working with the System.
+     * @throws Exception if unexpected errors appear.
      */
     @Test
     @LocalData
